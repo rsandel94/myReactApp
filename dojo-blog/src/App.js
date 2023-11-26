036a0cdd88469891cefe198bca234e9a3e501288
rsandel94 <robbiesandel@gmail.com>
2023-11-25 17:09:46 -0700

    formatted the page

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,17 +1,12 @@
-import './App.css';
 import NavBar from './Navbar';
-import Home from './>Home'
+import Home from './Home'
 
 function App() {
-  const title = 'Welcome to the new blog'
-  const likes = 50
-  const link = "https://www.google.com"
-
   return (
     <div className="App">
       <NavBar />
       <div className="content">
-        {/* <Home /> */}
+         <Home /> 
       </div>
     </div>
   );
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    
+    const handleClick = () => {
+        console.log('hello, ninjas');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name);
+    }
+
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick ={handleClick}>Click me</button>
+            <button onClick ={(e) => {handleClickAgain('mario')}}>Click me again</button>
         </div>
      );
 }
